17daf361f5f1d8f96ee3a8412b6d5d7e805f457e
khaiiilll <37998349+khaiiilll@users.noreply.github.com>
2018-04-19 13:41:38 +0800

    Create container.php

---
+++ container.php
@@ -0,0 +1,11 @@
+<div class="custom-container-57">
+  <div class="image-holder">
+    <div class="image-inner" style="background-image: url(<?php echo imgfolder('no-image.jpg'); ?>);"></div>
+  </div>
+  <div class="content-holder">
+    <div class="content-inner">
+      <h6>Heading</h6>
+      <h2>Content Here</h2>
+    </div>
+  </div>
+</div>
